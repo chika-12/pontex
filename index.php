eefb761e83b49f0c5cee8d20d813d5cf2d270468
chika-12 <markworship001@gmail.cm>
2022-07-04 15:48:05 +0100

    Index

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php 
+include_once 'header.php';
+?>
+
+<div class ='container'>
+<div class ='nav'>
+<form action = '' method ='post'>
+<div class=''>
+<label> 
+<input placeholder = 'surname' class ='input'/>
+</label> </p>
+</div>
+<label>
+<input placeholder = 'first name' class ='input'/>
+</label></p>
+
+<label>
+<input placeholder = 'middle name' class ='input'/>
+</label></p><label>
+
+<label>
+<input placeholder = 'email' class ='input'/>
+</label></p>
+
+<label>
+<input placeholder = 'user name'class ='input'/>
+</label></p>
+
+
+<label>
+<input placeholder = 'password' class ='input'/>
+</label></p>
+
+<button class='btn'>register</button>
+
+</form>
+</div>
+
+</div>
+<?php 
+include_once 'footer.php'; 
+?>
+
+
+
+
+
+
+
+ 
